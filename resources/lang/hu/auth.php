499a7b2f78e286e0fa6a9d228c1a8e0d588ac166
hubatruck <hubatruck2015@gmail.com>
2021-07-30 19:05:00 +0300

    Locale: Add hungarian translations

---
+++ resources/lang/hu/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Rossz felhasználónév és/vagy jelszó',
+    'password' => 'A megadott jelszó helytelen.',
+    'throttle' => 'Túl sok bejelentkezési kísérlet. Próbáld újra :seconds másodperc múlva.',
+
+];
