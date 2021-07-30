d68bfdcda0247b350934edf0f01c794800224bee
hubatruck <hubatruck2015@gmail.com>
2021-07-30 17:12:56 +0300

    Wallet: Add create/edit blade view component
    This component can be used to edit wallets.

---
+++ resources/views/wallet/edit.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ isset($wallet) ? 'Edit wallet' : 'Create wallet'}}</div>
+                    <div class="card-body">
+                        <form
+                            method="POST"
+                            action="{{ isset($wallet) ? route('wallet.data.update', ['id' => $wallet->id]) : route('wallet.data.create')}}"
+                            enctype="multipart/form-data"
+                            class="row"
+                        >
+                            @csrf
+
+                            <div class="col-md-12">
+                                <label for="name" class="form-label">Name</label>
+                                <input
+                                    id="name"
+                                    class="form-control @error('name') is-invalid @enderror"
+                                    name="name"
+                                    type="text"
+                                    value="{{ old('name', isset($wallet) ? $wallet->name : '') }}"
+                                />
+
+                                @error('name')
+                                <span class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-12">
+                                <label for="notes" class="form-label">Notes</label>
+                                <textarea
+                                    id="notes"
+                                    class="form-control @error('notes') is-invalid @enderror"
+                                    name="notes"
+                                >{{ old('notes', isset($wallet) ? $wallet->notes : '') }}</textarea>
+
+                                @error('notes')
+                                <span class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-12">
+                                <label for="balance" class="form-label">Balance</label>
+                                <input
+                                    id="balance"
+                                    class="form-control @error('balance') is-invalid @enderror"
+                                    name="balance"
+                                    type="number"
+                                    value="{{ old('balance', isset($wallet) ? $wallet->balance : 0) }}"
+                                    step="0.01"
+                                />
+
+                                @error('balance')
+                                <span class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-check">
+                                    <input
+                                        id="is_card"
+                                        class="form-check-input"
+                                        name="is_card"
+                                        type="checkbox"
+                                        {{ old('is_card', isset($wallet) && $wallet->is_card ? 'checked' : '') }}
+                                    >
+                                    <label class="form-check-label" for="is_card">Wallet represents credit card</label>
+                                </div>
+                            </div>
+
+                            <div class="col-12 mt-2">
+                                <button type="submit" class="btn btn-success">
+                                    {{ isset($wallet) ? 'Save' : 'Create' }}
+                                </button>
+                                <a type="submit" href="{{route('wallet.view.all')}}" class="btn btn-outline-danger">
+                                    Cancel
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
