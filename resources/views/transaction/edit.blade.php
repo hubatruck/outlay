cecbad08e4ce5cdbfb1e763f5d17ee7c0fa3b7a4
hubatruck <hubatruck2015@gmail.com>
2021-07-31 00:04:38 +0300

    Transactions: Add edit component/page
    This page is used to edit/create new transactions.

---
+++ resources/views/transaction/edit.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        {{ isset($transaction) ? __(':action transaction', ['action'=> __('Edit')]) : __(':action transaction', ['action' => __('Create')]) }}
+                    </div>
+                    <div class="card-body">
+                        <form
+                            method="POST"
+                            action="{{ isset($transaction) ? route('transaction.data.update', ['id' => $transaction->id]) : route('transaction.data.create') }}"
+                            enctype="multipart/form-data"
+                            class="row"
+                        >
+                            @csrf
+
+                            <div class="col-md-12">
+                                <label for="scope" class="form-label">{{ __('Scope') }}</label>
+                                <input
+                                    id="scope"
+                                    class="form-control @error('scope') is-invalid @enderror"
+                                    name="scope"
+                                    type="text"
+                                    value="{{ old('scope', isset($transaction) ? $transaction->scope : '') }}"
+                                />
+
+                                @error('scope')
+                                <span class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-12">
+                                <label for="amount" class="form-label">{{ __('Amount') }}</label>
+                                <input
+                                    id="amount"
+                                    class="form-control @error('balance') is-invalid @enderror"
+                                    name="amount"
+                                    type="number"
+                                    value="{{ old('amount', isset($transaction) ? $transaction->amount : 0) }}"
+                                    step="0.01"
+                                />
+
+                                @error('amount')
+                                <span class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+
+                            <div class=" col-md-12">
+                                <label for="wallet_id" class="form-label">{{ __('Wallet') }}</label>
+                                <select id="wallet_id" class="form-select form-control" name="wallet_id" required>
+                                    <option @if(!isset($transaction)) selected @endif disabled hidden value="">{{ __('Select...') }}</option>
+                                    @foreach(Auth::user()->wallets as $wallet)
+                                        <option
+                                            value="{{ $wallet->id }}"
+                                            @if(isset($transaction) && $wallet->id === $transaction->wallet_id)
+                                            selected
+                                            @endif
+                                        >
+                                            {{ $wallet->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class=" col-md-12">
+                                <label for="transaction_type_id" class="form-label">{{ __('Type') }}</label>
+                                <select id="transaction_type_id" class="form-select form-control" name="transaction_type_id"
+                                        required>
+                                    <option @if(!isset($transaction)) selected @endif disabled hidden value="">{{ __('Select...') }}</option>
+                                    @foreach(\App\Models\TransactionType::all() as $tt)
+                                        <option
+                                            value="{{ $tt->id }}"
+                                            @if(isset($transaction)&&$transaction->transactionType->id===$tt->id)
+                                            selected
+                                            @endif
+                                        >{{ __($tt->name) }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="col-12 mt-2">
+                                <button type="submit" class="btn btn-success">
+                                    {{ isset($transaction) ? __('Save') : __('Create') }}
+                                </button>
+                                <a type="submit" href="{{ route('transaction.view.all') }}"
+                                   class="btn btn-outline-danger">
+                                    {{ __('Cancel') }}
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
