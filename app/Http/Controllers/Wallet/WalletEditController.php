de06cb2f5fa0c3963d7cd464fe73a64c3cebdb9e
hubatruck <hubatruck2015@gmail.com>
2021-08-01 20:39:05 +0300

    style+docs: Reformat and add docs for functions for WalletEditController

--- app/Http/Controllers/Wallet/WalletEditController.php
+++ app/Http/Controllers/Wallet/WalletEditController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Wallet;
 
 use App\Http\Controllers\Controller;
 use App\Models\Wallet;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,63 +13,46 @@ use Illuminate\View\View;
 
 class WalletEditController extends Controller
 {
-    private $viewName = 'wallet/edit';
+    /**
+     * Editor view name
+     *
+     * @var string
+     */
+    private $editorViewName = 'wallet/edit';
 
+    /**
+     * Show the view for editing wallet
+     *
+     * @return View
+     */
     public function createView(): View
     {
-        return view($this->viewName);
+        return view($this->editorViewName);
     }
 
-    /// https://stackoverflow.com/a/59745972
+    /**
+     * Show the view for editing wallet
+     *
+     * @param string $id
+     * @return Application|Factory|\Illuminate\Contracts\View\View|RedirectResponse
+     */
     public function editView(string $id)
     {
         $wallet = Wallet::find($id);
         if (empty($wallet)) {
             return $this->walletDoesNotExist();
         }
-        if ($wallet->user_id !== Auth::user()->id) {
+        if ($wallet->user_id !== (Auth::user()->id ?? '-1')) {
             return $this->cannotEditWallet();
         }
-        return view($this->viewName, compact('wallet'));
+        return view($this->editorViewName, compact('wallet'));
     }
 
-    public function storeWallet(Request $request): RedirectResponse
-    {
-        $newWalletData = $this->validateRequest($request);
-        $newWalletData['user_id'] = Auth::user()->id;
-        Wallet::create($newWalletData);
-
-        return $this->redirectSuccess();
-    }
-
-    public function updateWallet(Request $request, string $id): RedirectResponse
-    {
-        $validated = $this->validateRequest($request);
-
-        $wallet = Wallet::find($id);
-
-        if (empty($wallet)) {
-            return $this->walletDoesNotExist();
-        }
-        if ($wallet->user_id !== Auth::user()->id) {
-            return $this->cannotEditWallet();
-        }
-
-        $wallet->fill($validated);
-        $wallet->save();
-        return $this->redirectSuccess('updated');
-    }
-
-    private function cannotEditWallet(): RedirectResponse
-    {
-        return redirect()
-            ->route('wallet.view.all')
-            ->with([
-                'message' => __('You cannot edit this wallet.'),
-                'status' => 'danger'
-            ]);
-    }
-
+    /**
+     * Redirect user to wallet list if wallet does not exist
+     *
+     * @return RedirectResponse
+     */
     private function walletDoesNotExist(): RedirectResponse
     {
         return redirect()
@@ -78,16 +63,42 @@ class WalletEditController extends Controller
             ]);
     }
 
-    private function redirectSuccess(string $successMethod = 'created'): RedirectResponse
+    /**
+     * Redirect user to wallet list, if user cannot edit wallet
+     *
+     * @return RedirectResponse
+     */
+    private function cannotEditWallet(): RedirectResponse
     {
         return redirect()
             ->route('wallet.view.all')
             ->with([
-                'message' => __('Wallet :action successfully.', ['action' => __($successMethod)]),
-                'status' => 'success'
+                'message' => __('You cannot edit this wallet.'),
+                'status' => 'danger'
             ]);
     }
 
+    /**
+     * Save a new wallet
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function storeWallet(Request $request): RedirectResponse
+    {
+        $newWalletData = $this->validateRequest($request);
+        $newWalletData['user_id'] = Auth::user()->id;
+        Wallet::create($newWalletData);
+
+        return $this->redirectSuccess();
+    }
+
+    /**
+     * Validate request data
+     *
+     * @param Request $request
+     * @return array
+     */
     public function validateRequest(Request $request): array
     {
         $data = $request->validate([
@@ -100,5 +111,46 @@ class WalletEditController extends Controller
 
         $data['is_card'] = isset($data['is_card']);
         return $data;
+    }
+
+    /**
+     * Redirect user to wallet list with success message
+     *
+     * @param string $successMethod
+     * @return RedirectResponse
+     */
+    private function redirectSuccess(string $successMethod = 'created'): RedirectResponse
+    {
+        return redirect()
+            ->route('wallet.view.all')
+            ->with([
+                'message' => __('Wallet :action successfully.', ['action' => __($successMethod)]),
+                'status' => 'success'
+            ]);
+    }
+
+    /**
+     * Update a wallet
+     *
+     * @param Request $request
+     * @param string $id
+     * @return RedirectResponse
+     */
+    public function updateWallet(Request $request, string $id): RedirectResponse
+    {
+        $validated = $this->validateRequest($request);
+
+        $wallet = Wallet::find($id);
+
+        if (empty($wallet)) {
+            return $this->walletDoesNotExist();
+        }
+        if ($wallet->user_id !== (Auth::user()->id ?? '-1')) {
+            return $this->cannotEditWallet();
+        }
+
+        $wallet->fill($validated);
+        $wallet->save();
+        return $this->redirectSuccess('updated');
     }
 }
