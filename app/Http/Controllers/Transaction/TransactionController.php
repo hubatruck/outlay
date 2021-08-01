3cdb59528b491d713a290b953e4d413bb9639211
hubatruck <hubatruck2015@gmail.com>
2021-08-01 17:47:16 +0300

    style: Rearrange TransactionController code

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -20,6 +20,16 @@ class TransactionController extends Controller
         return view($this->viewName);
     }
 
+    public function noWallet(): RedirectResponse
+    {
+        return redirect()
+            ->route('transaction.view.all')
+            ->with([
+                'message' => __('No wallet linked to account found.'),
+                'status' => 'danger'
+            ]);
+    }
+
     /// https://stackoverflow.com/a/59745972
     public function editView(string $id)
     {
@@ -37,12 +47,53 @@ class TransactionController extends Controller
         return view($this->viewName, compact('transaction'));
     }
 
+    private function transactionDoesNotExist(): RedirectResponse
+    {
+        return redirect()
+            ->route('transaction.view.all')
+            ->with([
+                'message' => __('Transaction does not exist.'),
+                'status' => 'danger'
+            ]);
+    }
+
+    private function cannotEditTransaction(): RedirectResponse
+    {
+        return redirect()
+            ->route('transaction.view.all')
+            ->with([
+                'message' => __('You cannot edit this transaction.'),
+                'status' => 'danger'
+            ]);
+    }
+
     public function storeTransaction(Request $request): RedirectResponse
     {
         $newTransactionData = $this->validateRequest($request);
         Transaction::create($newTransactionData);
 
         return $this->redirectSuccess();
+    }
+
+    public function validateRequest(Request $request): array
+    {
+        return $request->validate([
+            'wallet_id' => 'required|integer',
+            'scope' => 'required|max:255',
+            'amount' => 'numeric|max:999999.99',
+            'transaction_type_id' => 'required|integer',
+            'transaction_date' => 'required|date|date_format:Y-m-d',
+        ]);
+    }
+
+    private function redirectSuccess(string $successMethod = 'created'): RedirectResponse
+    {
+        return redirect()
+            ->route('transaction.view.all')
+            ->with([
+                'message' => __('Transaction :action successfully.', ['action' => __($successMethod)]),
+                'status' => 'success'
+            ]);
     }
 
     public function updateTransaction(Request $request, string $id): RedirectResponse
@@ -61,56 +112,5 @@ class TransactionController extends Controller
         $transaction->fill($validated);
         $transaction->save();
         return $this->redirectSuccess('updated');
-    }
-
-    private function cannotEditTransaction(): RedirectResponse
-    {
-        return redirect()
-            ->route('transaction.view.all')
-            ->with([
-                'message' => __('You cannot edit this transaction.'),
-                'status' => 'danger'
-            ]);
-    }
-
-    private function transactionDoesNotExist(): RedirectResponse
-    {
-        return redirect()
-            ->route('transaction.view.all')
-            ->with([
-                'message' => __('Transaction does not exist.'),
-                'status' => 'danger'
-            ]);
-    }
-
-    public function noWallet(): RedirectResponse
-    {
-        return redirect()
-            ->route('transaction.view.all')
-            ->with([
-                'message' => __('No wallet linked to account found.'),
-                'status' => 'danger'
-            ]);
-    }
-
-    private function redirectSuccess(string $successMethod = 'created'): RedirectResponse
-    {
-        return redirect()
-            ->route('transaction.view.all')
-            ->with([
-                'message' => __('Transaction :action successfully.', ['action' => __($successMethod)]),
-                'status' => 'success'
-            ]);
-    }
-
-    public function validateRequest(Request $request): array
-    {
-        return $request->validate([
-            'wallet_id' => 'required|integer',
-            'scope' => 'required|max:255',
-            'amount' => 'numeric|max:999999.99',
-            'transaction_type_id' => 'required|integer',
-            'transaction_date' => 'required|date|date_format:Y-m-d',
-        ]);
     }
 }
