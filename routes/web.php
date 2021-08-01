4c41b8289edf897562fe94bb23c12350ff800a24
hubatruck <hubatruck2015@gmail.com>
2021-08-01 14:17:59 +0300

    routing: Refactor editing routes and add groups

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\DataTables\TransactionsDataTable;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Transaction\TransactionController;
 use App\Http\Controllers\Wallet\WalletEditController;
 use Illuminate\Support\Facades\Auth;
@@ -27,27 +28,33 @@ Route::get('/', function () {
     return redirect('/home');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::prefix('wallets')->middleware(['auth'])->group(function () {
-    Route::get('/', function () {
-        return view('wallet/list');
-    })->name('wallet.view.all');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-    Route::get('/create', [WalletEditController::class, 'createView'])->name('wallet.view.create');
-    Route::get('/edit/{id}', [WalletEditController::class, 'editView'])->name('wallet.view.update');
-    Route::post('/create', [WalletEditController::class, 'storeWallet'])->name('wallet.data.create');
-    Route::post('/edit/{id}', [WalletEditController::class, 'updateWallet'])->name('wallet.data.update');
-});
+    Route::prefix('wallets')->group(function () {
+        Route::get('/', function () {
+            return view('wallet/list');
+        })->name('wallet.view.all');
 
+        Route::get('create', [WalletEditController::class, 'createView'])->name('wallet.view.create');
+        Route::post('create', [WalletEditController::class, 'storeWallet'])->name('wallet.data.create');
+        Route::prefix('{id}')->group(function () {
+            Route::get('edit', [WalletEditController::class, 'editView'])->name('wallet.view.update');
+            Route::post('edit', [WalletEditController::class, 'updateWallet'])->name('wallet.data.update');
+        });
+    });
 
-Route::prefix('transactions')->middleware(['auth'])->group(function () {
-    Route::get('/', function (TransactionsDataTable $dataTable) {
-        return $dataTable->render('transaction/list');
-    })->name('transaction.view.all');
+    Route::prefix('transactions')->group(function () {
+        Route::get('/', function (TransactionsDataTable $dataTable) {
+            return $dataTable->render('transaction/list');
+        })->name('transaction.view.all');
 
-    Route::get('/create', [TransactionController::class, 'createView'])->name('transaction.view.create');
-    Route::get('/edit/{id}', [TransactionController::class, 'editView'])->name('transaction.view.update');
-    Route::post('/create', [TransactionController::class, 'storeTransaction'])->name('transaction.data.create');
-    Route::post('/edit/{id}', [TransactionController::class, 'updateTransaction'])->name('transaction.data.update');
+        Route::get('create', [TransactionController::class, 'createView'])->name('transaction.view.create');
+        Route::post('create', [TransactionController::class, 'storeTransaction'])->name('transaction.data.create');
+        Route::prefix('{id}')->group(function () {
+            Route::get('edit', [TransactionController::class, 'editView'])->name('transaction.view.update');
+            Route::post('edit', [TransactionController::class, 'updateTransaction'])->name('transaction.data.update');
+        });
+    });
 });
