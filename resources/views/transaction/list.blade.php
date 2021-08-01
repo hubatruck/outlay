2e863d534f35e7c38baa54ae55834fd5d7efb243
hubatruck <hubatruck2015@gmail.com>
2021-08-01 13:33:00 +0300

    Transactions: Format no wallet error message displaying

--- resources/views/transaction/list.blade.php
+++ resources/views/transaction/list.blade.php
@@ -10,17 +10,20 @@
                 @if (!count(Auth::user()->wallets))
                     <div class="alert alert-warning">
                         {{ __('You don\'t have any wallet connected to you account, so the "transactions" feature is not available.') }}
-                        <a class="alert-link" href="{{ route('wallet.view.create') }}">{{ __('Create a wallet by clicking here.') }}</a>
+                        <a class="alert-link" href="{{ route('wallet.view.create') }}">
+                            {{ __('Create a wallet by clicking here.') }}
+                        </a>
                     </div>
                 @endif
                 <div class="card">
                     <div class="card-header">{{ __('Transactions') }}</div>
                     <div class="card-body">
-                        @if(count(Auth::user()->wallets))
-                            @if (count(Auth::user()->transactions))
-                                {{ $dataTable->table(['class' => 'table  table-response table-hover dt-responsive', 'width' => '100%']) }}
-                            @else
-                                {{ __('You don\'t have any transactions available.') }}
+                        @if (count(Auth::user()->transactions))
+                            {{ $dataTable->table(['class' => 'table  table-response table-hover dt-responsive', 'width' => '100%']) }}
+                        @else
+                            {{ __('You don\'t have any transactions available.') }}
+                            @if(count(Auth::user()->wallets))
+                                <a href="{{ route('transaction.view.create') }}">{{ __('Create') }}.</a>
                             @endif
                         @endif
                     </div>
