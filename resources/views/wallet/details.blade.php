75ef30a1861006e13e4517e5aa999305c765d172
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-02 22:18:22 +0300

    Wallet: Restyle details page

--- resources/views/wallet/details.blade.php
+++ resources/views/wallet/details.blade.php
@@ -14,14 +14,41 @@
                 @endif
                 <div class="card">
                     <div class="card-header">
-                        {{ __('Wallet details for :wallet', ['wallet' => $wallet->name ?? 'UNDEFINED']) }}
-                    </div>
-                    <div class="card-body">
-                        <div class="m-3 p-6 rounded shadow col-md-6">
-                            {!! $dailyChart->container() !!}
-                        </div>
-                        <div class="m-3 p-6 rounded shadow col-md-6">
-                            {!! $typeChart->container() !!}
+                        <h4>{{ __('Wallet details for :wallet', ['wallet' => $wallet->name ?? 'ERR:UNDEFINED']) }}</h4>
+                        <div class="card-body">
+                            <div class="card mb-2">
+                                <div class="card-header">
+                                    {{ __('Manage wallet') }}
+                                </div>
+                                <div class="card-body">
+                                    <a
+                                        class="btn btn-outline-success"
+                                        href="{{ route('wallet.view.update', ['id' => $wallet->id]) }}"
+                                    >
+                                        {{ __('Edit') }}
+                                    </a>
+                                    <a class="btn btn-outline-danger" href="#">{{ __('Delete') }}</a>
+                                </div>
+                            </div>
+                            @if (count($wallet->transactions))
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5>{{  __('Transaction charts for :month', ['month' => __(date('F'))]) }}</h5>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="col-12">
+                                            <div class="row">
+                                                <div class=" col-12 pt-3 mb-1 rounded shadow">
+                                                    {!! $dailyChart->container() !!}
+                                                </div>
+                                                <div class="col-md-6 p-3 rounded shadow">
+                                                    {{$typeChart->container() }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -31,7 +58,7 @@
 @endsection
 
 @push('scripts')
-    <script src="{{ $dailyChart->cdn() }}"></script>
+    <script src="{{ @asset('vendor/larapex-charts/apexcharts.js') }}"></script>
     {{ $dailyChart->script() }}
     {{ $typeChart->script() }}
 @endpush
