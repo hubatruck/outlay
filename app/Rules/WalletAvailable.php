058f9d57368210331ba562c7c63ddff294379ec7
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-03 00:17:18 +0300

    rules: Add rule for checking if wallet is active

---
+++ app/Rules/WalletAvailable.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Wallet;
+use Illuminate\Contracts\Validation\Rule;
+
+class WalletAvailable implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Check if the selected wallet is active
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        return Wallet::find($value) !== null ?? false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        return __('The selected wallet is not available.');
+    }
+}
