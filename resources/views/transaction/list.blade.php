c3d90fcf442989ef62bdff2ee382ad93aeb2e2eb
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-03 10:34:51 +0300

    Transactions: Display message if no active wallet is found

--- resources/views/transaction/list.blade.php
+++ resources/views/transaction/list.blade.php
@@ -14,6 +14,13 @@
                             {{ __('Create a wallet by clicking here.') }}
                         </a>
                     </div>
+                @elseif(!Auth::user()->hasAnyActiveWallet())
+                    <div class="alert alert-warning">
+                        {{ __('You don\'t have any wallet marked as active. Transaction creation is unavailable.') }}<br/>
+                        <a class="alert-link" href="{{ route('wallet.view.all') }}">
+                            {{ __('Activate a wallet by clicking here.') }}
+                        </a>
+                    </div>
                 @endif
                 <div class="card">
                     <div class="card-header">{{ __('Transactions') }}</div>
