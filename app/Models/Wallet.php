4d26c8a4a28156401fca58e194996a196557cece
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-05 18:09:46 +0300

    Wallet: Add function for checking wallet
    Same description goes here as for the transaction's similar function.

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use App\Feedbacks\WalletFeedback;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class Wallet extends Model
 {
@@ -17,6 +20,23 @@ class Wallet extends Model
      * @var string
      */
     protected $fillable = ['user_id', 'name', 'balance', 'notes', 'is_card'];
+
+    /**
+     * Check if the provided wallet is wallet or if the user owns the wallet
+     *
+     * @param Wallet|null $wallet
+     * @return RedirectResponse|null
+     */
+    public static function check(Wallet $wallet = null): ?RedirectResponse
+    {
+        if ($wallet === null) {
+            return WalletFeedback::existError();
+        }
+        if (!Auth::user()->owns($wallet)) {
+            return WalletFeedback::editError();
+        }
+        return null;
+    }
 
     /**
      * Get the user who owns this wallet
