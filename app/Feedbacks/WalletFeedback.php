7feb14553c2c46be78bda32f91137f9fed39bfc0
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-05 18:04:25 +0300

    Wallet: Add WalletFeedback class
    This class will be holding all the redirects with messages that should
    be displayed for the user on the screen. These shorthand are called 'feedbacks'.

---
+++ app/Feedbacks/WalletFeedback.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Feedbacks;
+
+use App\Models\Wallet;
+use Illuminate\Http\RedirectResponse;
+
+class WalletFeedback
+{
+    /**
+     * User cannot view the wallet.
+     * Type: error
+     *
+     * @return RedirectResponse
+     */
+    public static function viewError(): RedirectResponse
+    {
+        return redirect(route('wallet.view.all'))
+            ->with([
+                'message' => __('Error') . ': ' . __('You cannot view this wallet.'),
+                'status' => 'danger',
+            ]);
+    }
+
+    /**
+     * Redirect user to wallet list if wallet does not exist.
+     * Type: error
+     *
+     * @return RedirectResponse
+     */
+    public static function existError(): RedirectResponse
+    {
+        return redirect()
+            ->route('wallet.view.all')
+            ->with([
+                'message' => __('Error') . ': ' . __('Wallet does not exist.'),
+                'status' => 'danger',
+            ]);
+    }
+
+    /**
+     * Redirect user with 'no wallet found' error.
+     * Type: error
+     *
+     * @param string $type
+     * @param null $url
+     * @return RedirectResponse
+     */
+    public static function noWalletError(string $type = '', $url = null): RedirectResponse
+    {
+        $url = $url ?? previousUrlOr(route('transaction.view.all'));
+        return redirect($url)
+            ->with([
+                'message' => __('Error: ') . __(
+                        'No :type wallet linked to your account found.', [
+                            'type' => __($type),
+                        ]
+                    ),
+                'status' => 'danger',
+            ]);
+    }
+
+    /**
+     * Wallet cannot be used for quick transaction creation.
+     * Type: error
+     * @return RedirectResponse
+     */
+    public static function quickCreateError(): RedirectResponse
+    {
+        return redirect(route('wallet.view.all'))
+            ->with([
+                'status' => 'danger',
+                'message' => __('Error: ') . __('Wallet unavailable for quick transaction creation.'),
+            ]);
+    }
+
+    /**
+     * Wallet got transactions
+     * Type: error
+     *
+     * @param Wallet $wallet
+     * @return RedirectResponse
+     */
+    public static function hasTransactionsError(Wallet $wallet): RedirectResponse
+    {
+        return redirect(previousUrlOr(route('wallet.view.details', ['id' => $wallet->id])))
+            ->with([
+                'message' => __('Error') . ': ' . __('Wallet has transactions linked to it. Cannot be deleted.'),
+                'status' => 'danger',
+            ]);
+    }
+
+    /**
+     * Redirect user to wallet list, if user cannot edit wallet.
+     * Type: error
+     *
+     * @return RedirectResponse
+     */
+    public static function editError(): RedirectResponse
+    {
+        return redirect()
+            ->route('wallet.view.all')
+            ->with([
+                'message' => __('Error') . ': ' . __('You cannot edit this wallet.'),
+                'status' => 'danger',
+            ]);
+    }
+
+    /**
+     * Redirect user to wallet list with success message.
+     * Type: success
+     *
+     * @param string $successMethod
+     * @param string|null $url
+     * @return RedirectResponse
+     */
+    public function success(string $successMethod = 'created', string $url = null): RedirectResponse
+    {
+        return redirect($url ?? route('wallet.view.all'))
+            ->with([
+                'message' => __(
+                    'Wallet :action successfully.', [
+                        'action' => __($successMethod)
+                    ]
+                ),
+                'status' => 'success',
+            ]);
+    }
+}
