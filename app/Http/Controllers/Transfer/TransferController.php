1b643f799246a553f3d5afc2d40e9ce36477ed01
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-09 17:31:17 +0300

    Transfers: Create TransferController

---
+++ app/Http/Controllers/Transfer/TransferController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Transfer;
+
+use App\Feedbacks\TransferFeedback;
+use App\Feedbacks\WalletFeedback;
+use App\Http\Controllers\Controller;
+use App\Models\Transfer;
+use App\Rules\UserOwnsWalletRule;
+use App\Rules\WalletAvailable;
+use App\Rules\WalletExistsRule;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TransferController extends Controller
+{
+    /**
+     * Show the create a transfer view
+     *
+     * @return Application|Factory|View|RedirectResponse
+     */
+    public function createView()
+    {
+        if (!Auth::user()->hasAnyActiveWallet()) {
+            return WalletFeedback::noWalletError(
+                Auth::user()->hasWallet() ? 'active' : '',
+                route('transfer.view.all')
+            );
+        }
+        return view('transfer.create');
+    }
+
+    /**
+     * Store a transfer in the database
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function storeTransfer(Request $request): RedirectResponse
+    {
+        $transfer = $this->validateRequest($request);
+        Transfer::create($transfer);
+
+        return TransferFeedback::success();
+    }
+
+    /**
+     * Validate request data
+     *
+     * @param Request $request
+     * @return array
+     */
+    private function validateRequest(Request $request): array
+    {
+        return $request->validate([
+            'description' => 'required|string|max:255',
+            'from_wallet_id' => ['required', new UserOwnsWalletRule(), new WalletAvailable()],
+            'to_wallet_id' => ['required', 'different:from_wallet_id', new WalletExistsRule()],
+            'amount' => 'numeric|min:0.01|max:999999.99',
+            'transfer_date' => 'required|date|date_format:Y-m-d|before_or_equal:' . date('Y-m-d'),
+        ]);
+    }
+}
