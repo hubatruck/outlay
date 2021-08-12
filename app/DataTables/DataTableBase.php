a9e42908faea83483cc21e71268fe2db7412ca8e
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-12 16:24:57 +0300

    DataTables: Add preliminary support for date ranges
    Instead of manually searching dates, the user will provide a date range
    for the data to be filtered in.

--- app/DataTables/DataTableBase.php
+++ app/DataTables/DataTableBase.php
@@ -4,6 +4,8 @@ namespace App\DataTables;
 
 use Carbon\Carbon;
 use Carbon\Exceptions\InvalidFormatException;
+use ErrorException;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Yajra\DataTables\Html\Builder;
 use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Services\DataTable;
@@ -38,6 +40,81 @@ abstract class DataTableBase extends DataTable
 //            });
 //        });
     }";
+
+    /**
+     * Fields that should be handled as dates
+     *
+     * @var array
+     */
+    protected $dateColumns = [];
+
+    /**
+     * Apply custom filters to DB query
+     *
+     * @return HasManyThrough|\Illuminate\Database\Query\Builder|\Illuminate\Database\Schema\Builder
+     */
+    public function query()
+    {
+        $query = $this->queryBase();
+        $range = $this->parseDateRange();
+
+        if ($range) {
+            $this->applyDateRange($query, $this->dateColumns, $range);
+        }
+
+        return $query;
+    }
+
+    /**
+     * Query for getting data from the database
+     *
+     * @return HasManyThrough|\Illuminate\Database\Query\Builder|\Illuminate\Database\Schema\Builder
+     */
+    abstract protected function queryBase();
+
+    /**
+     * Parse data range from request
+     *
+     * @return array
+     */
+    private function parseDateRange(): array
+    {
+        $reqDateRange = $this->request()->get('date_range');
+        $dateRange = [];
+
+        if ($reqDateRange) {
+            $format = 'Y-m-d H:i:s';
+
+            try {
+                [$from, $to] = explode(' to ', $reqDateRange);
+            } catch (ErrorException $e) {
+                /// In case the user selects a single day
+                $to = $from = $reqDateRange;
+            }
+
+            try {
+                $dateRange[] = Carbon::parse($from)->startOfDay()->format($format);
+                $dateRange[] = Carbon::parse($to)->endOfDay()->format($format);
+            } catch (InvalidFormatException $e) {
+                $dateRange = [];
+            }
+        }
+        return $dateRange;
+    }
+
+    /**
+     * Apply between query for specified columns
+     *
+     * @param $query
+     * @param array $columns
+     * @param array $range Format: ['YYYY-MM-DD HH:II:SS', 'YYYY-MM-DD HH:II:SS']
+     */
+    private function applyDateRange($query, array $columns, array $range): void
+    {
+        foreach ($columns as $col) {
+            $query->whereBetween($col, $range);
+        }
+    }
 
     protected function getButtons($addCreateButton = true): array
     {
