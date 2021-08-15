f993782aa3e35b21cd9c19335ae2a5f0853ea70f
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-15 17:06:15 +0300

    Transactions: Convert list view to UIkit3

--- resources/views/transaction/list.blade.php
+++ resources/views/transaction/list.blade.php
@@ -1,45 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                @if (session('message'))
-                    <div class="alert alert-{{ session('status') }}">{{ session('message') }}</div>
-                @endif
-                @if (!Auth::user()->hasWallet())
-                    <div class="alert alert-warning">
-                        {{ __('You don\'t have any wallet connected to you account. Transactions feature is not available.') }}
-                        <br/>
-                        <a class="alert-link" href="{{ route('wallet.view.create') }}">
-                            {{ __('Create a wallet by clicking here.') }}
-                        </a>
-                    </div>
-                @elseif(!Auth::user()->hasAnyActiveWallet())
-                    <div class="alert alert-warning">
-                        {{ __('You don\'t have any wallet marked as active. Transaction creation is unavailable.') }}
-                        <br/>
-                        <a class="alert-link" href="{{ route('wallet.view.all') }}">
-                            {{ __('Activate a wallet by clicking here.') }}
-                        </a>
-                    </div>
-                @endif
-                <div class="card">
-                    <div class="card-header">{{ __('Transactions') }}</div>
-                    <div class="card-body">
-                        @if (Auth::user()->hasTransactions())
-                            {!! $dataTable->table(['class' => 'table table-response table-hover dt-responsive', 'width' => '100%'], true) !!}
-                        @else
-                            {{ __('Nothing here...') }}
-                            @if(Auth::user()->hasAnyActiveWallet())
-                                <a href="{{ route('transaction.view.create') }}">{{ __('Create') }}.</a>
-                            @endif
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+  <x-page-title>{{ __('Transactions') }}</x-page-title>
+  <div class="uk-card-body">
+    @if (!Auth::user()->hasWallet())
+      <div class="alert alert-warning">
+        {{ __('You don\'t have any wallet connected to you account. Transactions feature is not available.') }}
+        <br/>
+        <a class="alert-link" href="{{ route('wallet.view.create') }}">
+          {{ __('Create a wallet by clicking here.') }}
+        </a>
+      </div>
+    @elseif(!Auth::user()->hasAnyActiveWallet())
+      <div class="alert alert-warning">
+        {{ __('You don\'t have any wallet marked as active. Transaction creation is unavailable.') }}
+        <br/>
+        <a class="alert-link" href="{{ route('wallet.view.all') }}">
+          {{ __('Activate a wallet by clicking here.') }}
+        </a>
+      </div>
+    @endif
+    @if (Auth::user()->hasTransactions())
+      {!! $dataTable->table(['class' => 'uk-table uk-table-response uk-table-divider uk-table-hover dt-responsive uk-margin-remove', 'width' => '100%'], true) !!}
+    @else
+      {{ __('Nothing here...') }}
+      @if(Auth::user()->hasAnyActiveWallet())
+        <a href="{{ route('transaction.view.create') }}">{{ __('Create') }}.</a>
+      @endif
+    @endif
+  </div>
 @endsection
 
 @push('scripts')
