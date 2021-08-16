cd26e0b4f4881de90a00a1fe414b6d58e472411b
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-16 12:11:23 +0300

    helpers: Create function for adding messages
    This function can be used to add messages that we want to show to the
    user once.

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -15,3 +15,30 @@ if (!function_exists('previousUrlOr')) {
         return ($previous === $current || $previous === $login) ? $fallback : $previous;
     }
 }
+
+if (!function_exists('addSessionMsg')) {
+    /**
+     * Add a message to be displayed on screen
+     *
+     * @param array $message
+     * @param bool $now Set to true, if you are adding a message without redirecting, just showing a view
+     */
+    function addSessionMsg(array $message, bool $now = false): void
+    {
+        if (session('messages')) {
+            $messages = session('messages');
+        } else {
+            $messages = [];
+        }
+
+        $messages[] = $message;
+        $messages = array_unique($messages, SORT_REGULAR);
+
+        if ($now) {
+            /// https://stackoverflow.com/a/31743850
+            session()->now('messages', $messages);
+        } else {
+            session()->flash('messages', $messages);
+        }
+    }
+}
