51edc19541307a60966db06613061aaf108b8a2e
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-18 12:26:14 +0300

    Misc: Convert regular date inputs into Flatpickr instances

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+/// Convert regular date inputs into flatpickr date inputs
+$('input[type=date]').each(function () {
+    /// ignore DataTables input, they are handled separately
+    if (this.id.match('dtc') === null) {
+        this.flatpickr({
+            altInput: true,
+            altInputClass: 'uk-input',
+            locale: document.documentElement.lang,
+        });
+    }
+});
