066f7474c9aee6b4c48a894fd4a67ec2a8a10dc5
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-18 22:15:26 +0300

    Charts: Add DataHandler classes
    These classes will be used to transform and manipulate data used by the
    charts.
    All previous transformation methods from the chart classes should be
    extracted to DataHandlers instead of polluting with processing function.

---
+++ app/DataHandlers/BalanceChartDataHandler.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\DataHandlers;
+
+use Illuminate\Support\Collection;
+
+class BalanceChartDataHandler extends ChartDataHandler
+{
+    /**
+     * Creates a new instance of this class
+     *
+     * @param Collection|array|null $data
+     * @return ChartDataHandler
+     */
+    protected static function newInstance($data = null): BalanceChartDataHandler
+    {
+        return new BalanceChartDataHandler($data);
+    }
+
+    /**
+     * Join instance with another one
+     * Note: Data keys should be the same.
+     *
+     * @param BalanceChartDataHandler $other
+     * @return $this
+     */
+    public function with(BalanceChartDataHandler $other): BalanceChartDataHandler
+    {
+        foreach ($this->data as $day => $amount) {
+            $this->data[$day] += $other->data[$day];
+        }
+        return $this;
+    }
+
+
+    /**
+     * Offset the balance data, so it will be displayed correctly
+     * This is needed, because otherwise previous month's balances won't
+     * be taken into account.
+     *
+     * @param int $walletBalance Balance of the wallet, as of today
+     * @return $this
+     */
+    public function offsetBalance(int $walletBalance): BalanceChartDataHandler
+    {
+        $offset = $walletBalance - $this->data[array_key_last($this->data)];
+
+        foreach ($this->data as &$item) {
+            $item += $offset;
+        }
+        return $this;
+    }
+
+    /**
+     * Instead of showing a per day balance, make the balance continuous.
+     *
+     * @return $this
+     */
+    public function sumWithPreviousDays(): BalanceChartDataHandler
+    {
+        $keys = array_keys($this->data);
+        for ($i = 1; $i < sizeof($this->data); $i++) {
+            $this->data[$keys[$i]] += $this->data[$keys[$i - 1]];
+        }
+        return $this;
+    }
+}
---
+++ app/DataHandlers/ChartDataHandler.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\DataHandlers;
+
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Illuminate\Support\Collection;
+
+class ChartDataHandler
+{
+    /**
+     * Date format used to display dates
+     */
+    public const DATE_FORMAT = 'Y-m-d';
+
+    /**
+     * Stored data
+     *
+     * @var array|Collection|null
+     */
+    protected $data;
+
+    /**
+     * Floating point digit precision
+     *
+     * @var int
+     */
+    protected int $dataPrecision;
+
+    /**
+     * @param array|Collection|null $data
+     * @param int $dataPrecision
+     */
+    public function __construct($data = null, int $dataPrecision = 2)
+    {
+        if ($data instanceof Collection) {
+            $data = $data->toArray();
+        }
+        $this->data = $data;
+        $this->dataPrecision = $dataPrecision;
+    }
+
+    /**
+     * Create a new instance from a give data
+     * This function eases usage of the class, by adding the ability to call statically the constructor,
+     * instead of going the regular $someVar = new ChartDataHandler(...) way
+     *
+     * @param Collection|array|null $data
+     * @return ChartDataHandler
+     */
+    public static function from($data = null): ChartDataHandler
+    {
+        return static::newInstance($data);
+    }
+
+    /**
+     * Creates a new instance of this class
+     *
+     * @param Collection|array|null $data
+     * @return ChartDataHandler
+     */
+    protected static function newInstance($data = null): ChartDataHandler
+    {
+        return new ChartDataHandler($data);
+    }
+
+    /**
+     * Set the floating point precision
+     *
+     * @param int|mixed $dataPrecision
+     */
+    public function setDataPrecision($dataPrecision): void
+    {
+        $this->dataPrecision = $dataPrecision;
+    }
+
+    /**
+     * Shorter alias function for `$this->reduceDataPrecision()->get()`
+     * @return array
+     */
+    public function reduceDPAndGet(): array
+    {
+        return $this->reduceDataPrecision()->get();
+    }
+
+    /**
+     * Get the data
+     *
+     * @return array
+     */
+    public function get(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * Reduces the precision of the stored data
+     *
+     * @return $this
+     */
+    public function reduceDataPrecision(): ChartDataHandler
+    {
+        $cutter = 10 ** $this->dataPrecision;
+        $this->data = array_map(static function ($item) use ($cutter) {
+            return floor($item * $cutter) / $cutter;
+        }, $this->data);
+        return $this;
+    }
+
+    /**
+     * Translate each data item, if possible
+     *
+     * @return $this
+     */
+    public function translate(): ChartDataHandler
+    {
+        $this->data = array_map(static function ($item) {
+            return __($item);
+        }, $this->data);
+        return $this;
+    }
+
+    /**
+     * Fill out the keys of the array, so each day of the month is present
+     *
+     * @return $this
+     */
+    public function addMissingDays(): ChartDataHandler
+    {
+        $cutter = 10 ** $this->dataPrecision;
+        $this->data = $this->eachDayOfTheMonth(function (Carbon $date) use ($cutter) {
+            return floor(($this->data[$date->format(self::DATE_FORMAT)] ?? 0) * $cutter) / $cutter;
+        });
+        return $this;
+    }
+
+    /**
+     * Do something with each day of the month
+     *
+     * @param callable $transformerCallback
+     * @return array
+     */
+    protected function eachDayOfTheMonth(callable $transformerCallback): array
+    {
+        $newData = [];
+        $period = CarbonPeriod::create(date('Y-m-01'), $this->lastDate());
+        foreach ($period as $day) {
+            $newData[] = $transformerCallback($day);
+        }
+
+        return $newData;
+    }
+
+    /**
+     * Which day should be the last day of the month, that we display
+     *
+     * @return string
+     */
+    protected function lastDate(): string
+    {
+        return date('Y-m-d');
+    }
+
+    /**
+     * Fill data array with the days of the month
+     *
+     * @return $this
+     */
+    public function daysOfMonth(): ChartDataHandler
+    {
+        $this->data = $this->eachDayOfTheMonth(function (Carbon $date) {
+            return $date->format(self::DATE_FORMAT);
+        });
+        return $this;
+    }
+}
