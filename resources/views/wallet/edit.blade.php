ffa5850375c684f1dd968cfb6025b5ec70816121
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-18 11:05:08 +0300

    Wallet: Convert edit/create page to UIkit3

--- resources/views/wallet/edit.blade.php
+++ resources/views/wallet/edit.blade.php
@@ -1,104 +1,99 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        {{ isset($wallet) ? __(':action wallet', ['action'=> __('Edit')]) : __(':action wallet', ['action' => __('Create')]) }}
-                    </div>
-                    <div class="card-body">
-                        <form
-                            method="POST"
-                            action="{{ isset($wallet) ? route('wallet.data.update', ['id' => $wallet->id]) : route('wallet.data.create') }}"
-                            enctype="multipart/form-data"
-                            class="row"
-                        >
-                            @csrf
+  <x-page-title>{{ isset($wallet) ? __(':action wallet', ['action'=> __('Edit')]) : __(':action wallet', ['action' => __('Create')]) }}</x-page-title>
+  <div class="uk-card-body">
+    <form
+      method="POST"
+      action="{{ isset($wallet) ? route('wallet.data.update', ['id' => $wallet->id]) : route('wallet.data.create') }}"
+      enctype="multipart/form-data"
+      class="uk-form-stacked"
+    >
+      @csrf
 
-                            <div class="col-md-12">
-                                <label for="name" class="form-label">{{ __('Name') }}<span class="text-danger">*</span></label>
-                                <input
-                                    id="name"
-                                    class="form-control @error('name') is-invalid @enderror"
-                                    name="name"
-                                    type="text"
-                                    value="{{ old('name', isset($wallet) ? $wallet->name : '') }}"
-                                    required
-                                />
-
-                                @error('name')
-                                <span class="invalid-feedback">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-
-                            <div class="col-md-12">
-                                <label for="notes" class="form-label">{{ __('Notes') }}</label>
-                                <textarea
-                                    id="notes"
-                                    class="form-control @error('notes') is-invalid @enderror"
-                                    name="notes"
-                                    placeholder="{{ __('This field is optional.') }}"
-                                >{{ old('notes', isset($wallet) ? $wallet->notes : '') }}</textarea>
-
-                                @error('notes')
-                                <span class="invalid-feedback">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                            <div class="col-md-12">
-                                <label for="balance" class="form-label">{{ __('Balance') }}</label>
-                                <input
-                                    id="balance"
-                                    class="form-control @error('balance') is-invalid @enderror"
-                                    name="balance"
-                                    type="number"
-                                    value="{{ old('balance', isset($wallet) ? $wallet->balance : 0) }}"
-                                    step="0.01"
-                                />
-
-                                @error('balance')
-                                <span class="invalid-feedback">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-check">
-                                    <input
-                                        id="is_card"
-                                        class="form-check-input"
-                                        name="is_card"
-                                        type="checkbox"
-                                        {{ old('is_card', isset($wallet) && $wallet->is_card ? 'checked' : '') }}
-                                    >
-                                    <label class="form-check-label" for="is_card">
-                                        {{ __('Wallet represents credit card') }}
-                                    </label>
-                                </div>
-                            </div>
-
-                            <div class="col-12 text-danger">{{ __('Fields marked with * are required.') }}</div>
-                            <div class="col-12 mt-2">
-                                <button type="submit" class="btn btn-success">
-                                    {{ isset($wallet) ? __('Save') : __('Create') }}
-                                </button>
-                                <a
-                                    type="submit"
-                                    href="{{ previousUrlOr(isset($wallet) ? route('wallet.view.details', ['id' => $wallet->id]) : route('wallet.view.all')) }}"
-                                    class="btn btn-outline-danger"
-                                >
-                                    {{ __('Cancel') }}
-                                </a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+      <div class="uk-margin">
+        <label for="name" class="uk-form-label">{{ __('Name') }}<span class="uk-text-danger">*</span></label>
+        <div class="uk-form controls">
+          <input
+            id="name"
+            class="uk-input @error('name')uk-form-danger @enderror"
+            name="name"
+            type="text"
+            value="{{ old('name', isset($wallet) ? $wallet->name : '') }}"
+            required
+          />
         </div>
-    </div>
+        @error('name')
+        <span class="uk-text-danger uk-text-small">
+          <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+      </div>
+
+      <div class="uk-margin">
+        <label for="notes" class="uk-form-label">{{ __('Notes') }}</label>
+        <div class="form-controls">
+        <textarea
+          id="notes"
+          class="uk-textarea @error('notes')uk-form-danger @enderror"
+          name="notes"
+          placeholder="{{ __('This field is optional.') }}"
+        >{{ old('notes', isset($wallet) ? $wallet->notes : '') }}</textarea>
+        </div>
+        @error('notes')
+        <span class="uk-text-danger uk-text-small">
+          <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+      </div>
+
+      <div class="uk-margin">
+        <label for="balance" class="uk-form-label">{{ __('Balance') }}</label>
+        <div class="uk-form controls">
+          <input
+            id="balance"
+            class="uk-input @error('balance')uk-form-danger @enderror"
+            name="balance"
+            type="number"
+            value="{{ old('balance', isset($wallet) ? $wallet->balance : 0) }}"
+            step="0.01"
+          />
+        </div>
+        @error('balance')
+        <span class="uk-text-danger uk-text-small">
+          <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+      </div>
+
+      <div class="uk-margin">
+        <div class="uk-form-controls uk-form-controls-text">
+          <label>
+            <input
+              id="is_card"
+              class="uk-checkbox"
+              name="is_card"
+              type="checkbox"
+              {{ old('is_card', isset($wallet) && $wallet->is_card ? 'checked' : '') }}
+            >
+            {{ __('Wallet represents credit card') }}
+          </label>
+        </div>
+      </div>
+
+      <div class="uk-text-danger">{{ __('Fields marked with * are required.') }}</div>
+      <div class="uk-margin-small-top">
+        <button type="submit" class="uk-button uk-button-primary">
+          {{ isset($wallet) ? __('Save') : __('Create') }}
+        </button>
+        <a
+          type="submit"
+          href="{{ previousUrlOr(isset($wallet) ? route('wallet.view.details', ['id' => $wallet->id]) : route('wallet.view.all')) }}"
+          class="uk-button uk-button-danger"
+        >
+          {{ __('Cancel') }}
+        </a>
+      </div>
+    </form>
+  </div>
 @endsection
