7df759ccabf49edc140a4807c816365ff9484c1f
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-18 12:07:22 +0300

    UI: Fix login page styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
-  <section class="uk-section">
-    <div class="uk-container">
-      <div class="uk-flex-center" uk-grid>
-        <div class="uk-width-large">
-          <x-validation-errors/>
+  <div class="uk-container">
+    <div class="uk-flex-center uk-padding-medium" uk-grid>
+      <div class="uk-width-large">
+        <x-validation-errors/>
 
-          <div class="uk-card uk-box-shadow-small">
-            <div class="uk-card-header">
-              <h2 class="uk-text-center">{{ __('Login') }}</h2>
+        <div class="uk-card-header">
+          <h2 class="uk-text-center">{{ __('Login') }}</h2>
+        </div>
 
-              <hr>
+        <div class="uk-card-body uk-padding-remove-top">
+          <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div class="uk-margin">
+              <label class="uk-form-label">{{ __('Username') }}</label>
+              <div class="uk-form-controls">
+                <div class="uk-inline uk-width-1-1">
+                  <span class="uk-form-icon" uk-icon="mail"></span>
+                  <input class="uk-input" type="name" name="name" value="{{ old('name') }}" required autofocus/>
+                </div>
+              </div>
             </div>
 
-            <div class="uk-card-body uk-padding-remove-top">
-              <form method="POST" action="{{ route('login') }}">
-                @csrf
-
-                <div class="uk-margin">
-                  <label class="uk-form-label">{{ __('Username') }}</label>
-
-                  <div class="uk-inline uk-width-1-1">
-                    <span class="uk-form-icon" uk-icon="icon: mail"></span>
-                    <input type="name" name="name" value="{{ old('name') }}" required autofocus/>
-                  </div>
+            <div class="uk-margin">
+              <label class="uk-form-label">{{ __('Password') }}</label>
+              <div class="uk-form-controls">
+                <div class="uk-inline uk-width-1-1">
+                  <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                  <input class="uk-input" type="password" name="password" required autocomplete="current-password"/>
                 </div>
-
-                <div class="uk-margin">
-                  <label class="uk-form-label">{{ __('Password') }}</label>
-
-                  <div class="uk-inline uk-width-1-1">
-                    <span class="uk-form-icon" uk-icon="icon: lock"></span>
-                    <input type="password" name="password" required autocomplete="current-password"/>
-                  </div>
-                </div>
-
-                <div class="uk-margin">
-                  <label>
-                    <input class="uk-checkbox" type="checkbox" name="remember" id="remember"
-                      {{ old('remember') ? 'checked' : '' }}>
-                    {{ __('Remember Me') }}
-                  </label>
-                </div>
-
-                <div class="uk-margin">
-                  <button class="uk-button uk-button-primary uk-width-1-1" type="submit">
-                    {{ __('Login') }}
-                  </button>
-                </div>
-
-                @if (Route::has('password.request'))
-                  <div class="uk-flex uk-flex-center">
-                    <a href="{{ route('password.request') }}">
-                      <small>{{ __('Forgot your password?') }}</small>
-                    </a>
-                    <span class="uk-margin-small-left uk-margin-small-right">|</span>
-                    <a href="{{ route('register') }}">
-                      <small>{{ __('Sign up') }}</small>
-                    </a>
-                  </div>
-                @endif
-              </form>
+              </div>
             </div>
-          </div>
+
+            <div class="uk-margin">
+              <label>
+                <input class="uk-checkbox" type="checkbox" name="remember" id="remember"
+                  {{ old('remember') ? 'checked' : '' }}>
+                {{ __('Remember Me') }}
+              </label>
+            </div>
+
+            <div class="uk-margin">
+              <button class="uk-button uk-button-primary uk-width-1-1" type="submit">
+                {{ __('Login') }}
+              </button>
+            </div>
+
+            @if (Route::has('password.request'))
+              <div class="uk-flex uk-flex-center">
+                <a href="{{ route('password.request') }}">
+                  <small>{{ __('Forgot your password?') }}</small>
+                </a>
+                <span class="uk-margin-small-left uk-margin-small-right">|</span>
+                <a href="{{ route('register') }}">
+                  <small>{{ __('Sign up') }}</small>
+                </a>
+              </div>
+            @endif
+          </form>
         </div>
       </div>
     </div>
-  </section>
+  </div>
 @endsection
