c4b67fc54da4997484a0af7edd8d41456374a056
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-08-18 12:26:34 +0300

    Transactions: Add colors to action buttons in list view

--- resources/views/components/transaction-dt-actions.blade.php
+++ resources/views/components/transaction-dt-actions.blade.php
@@ -4,12 +4,12 @@
 <div class="uk-padding-small" uk-dropdown="mode: click">
   <ul class="uk-nav uk-dropdown-nav">
     <li>
-      <a class="uk-button uk-button-small" href="{{ $editURL }}">
+      <a class="uk-button uk-button-small uk-text-success" href="{{ $editURL }}">
         <span uk-icon="pencil"></span>{{ __('Edit') }}
       </a>
     </li>
     <li>
-      <a class="uk-button uk-button-small" href="{{ $deleteURL }}">
+      <a class="uk-button uk-button-small uk-text-danger" href="{{ $deleteURL }}">
         <span uk-icon="trash"></span>{{ __('Delete') }}
       </a>
     </li>
