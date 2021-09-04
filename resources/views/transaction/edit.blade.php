3fd7c0e5be61c4a8037f61be272ce74fce3bc8a6
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-09-04 15:30:15 +0300

    Forms: Extract wallet select for transaction editor

--- resources/views/transaction/edit.blade.php
+++ resources/views/transaction/edit.blade.php
@@ -20,52 +20,7 @@
       </div>
 
       <div class="uk-margin">
-        <label for="wallet_id" class="uk-form-label">
-          {{ __('Wallet') }}<span class="uk-text-danger">*</span></label>
-        <div class="uk-form-controls">
-          <select
-            id="wallet_id"
-            class="uk-select @error('wallet_id')uk-form-danger @enderror"
-            name="wallet_id"
-            @if (Auth::user()->hasAnyActiveWallet())
-            required
-            @else
-            disabled
-            @endif
-          >
-            <option @if(!isset($transaction)) selected @endif disabled hidden value="">
-              {{ __('Select...') }}
-            </option>
-            @foreach(Auth::user()->wallets as $wallet)
-              @if ($wallet->deleted_at === null
-                  || (isset($transaction) && $transaction->wallet_id === $wallet->id))
-                <option
-                  value="{{ $wallet->id }}"
-                  @if((isset($transaction) && $wallet->id === $transaction->wallet_id)
-                      || ($selected_wallet_id ?? '') === (string) $wallet->id
-                      || (old('wallet_id') && (string) $wallet->id === old('wallet_id')))
-                  selected
-                  @endif
-                  @if ($wallet->deleted_at !== null)
-                  disabled
-                  @endif
-                >
-                  {{ $wallet->name }}
-                </option>
-              @endif
-            @endforeach
-          </select>
-          @if (!Auth::user()->hasAnyActiveWallet())
-            <span class="uk-alert-primary uk-padding-remove">
-              {{ __('No active wallets; this field cannot be changed.') }}
-            </span>
-          @endif
-        </div>
-        @error('wallet_id')
-        <span class="uk-text-danger uk-text-small">
-          <strong>{{ $message }}</strong>
-        </span>
-        @enderror
+        <x-forms.transaction-wallet-select :transaction="$transaction ?? null"/>
       </div>
 
       <div class="uk-margin">
