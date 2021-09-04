1126c49e560aef99ccf6b4813c072509d8853872
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-09-04 23:55:11 +0300

    DataTables: Customize print page title

--- resources/views/vendor/datatables/print.blade.php
+++ resources/views/vendor/datatables/print.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Print Table</title>
+  <title>{{ config('app.name') }} - {{ __('Printing') }}</title>
   <meta charset="UTF-8">
   <meta name=description content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
