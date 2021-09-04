3aa33500536c88104d2d011a03effdf36806e33d
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-09-04 17:51:04 +0300

    DataTables: Use lightweight stylesheet for print page
    We don't need to include a 10k line stylesheet when we are printing data,
    as many of those styles aren't used at all.

--- resources/views/vendor/datatables/print.blade.php
+++ resources/views/vendor/datatables/print.blade.php
@@ -5,8 +5,7 @@
   <meta charset="UTF-8">
   <meta name=description content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}"/>
+  <link rel="stylesheet" href="{{ mix('css/print.css') }}"/>
   <style>
     body {
       margin: 20px
