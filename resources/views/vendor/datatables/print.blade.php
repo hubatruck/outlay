536632981db1e4f813f1c4c3713a5b8b98c651c6
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-09-05 00:34:22 +0300

    DataTables: Change print page title (once again)
    Made the title of the page more generic, as the print page title is the
    same as the PDF export title.

--- resources/views/vendor/datatables/print.blade.php
+++ resources/views/vendor/datatables/print.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>{{ config('app.name') }} - {{ __('Printing') }}</title>
+  <title>{{ config('app.name') }} | {{ __('Export') }} - {{ now() }}</title>
   <meta charset="UTF-8">
   <meta name=description content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
