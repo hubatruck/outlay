1831ea20c1a5e51db0450f4ebb63638f86f21f64
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-09-05 21:06:53 +0300

    Forms: Add reset to today button for date picker

--- resources/views/components/forms/date-picker.blade.php
+++ resources/views/components/forms/date-picker.blade.php
@@ -1,17 +1,35 @@
+@php
+  use Carbon\Carbon;
+  $value = Carbon::parse(old($fieldName, $defaultValue ?? Carbon::now()))->format(globalDateFormat())
+@endphp
 <label for="transfer_date" class="uk-form-label">
   {{ __('Date') }}<span class="uk-text-danger">*</span>
 </label>
-<div class="uk-form-controls">
+<div class="uk-form-controls uk-inline uk-width-expand">
+  <a
+    class="uk-form-icon uk-form-icon-flip"
+    uk-icon="home"
+    onclick="clearDatePicker()"
+    uk-tooltip="{{ __('Reset range') }}"
+  ></a>
   <input
     id="{{ $fieldName }}"
     class="uk-input @error($fieldName) uk-form-danger @enderror"
     name="{{ $fieldName }}"
     type="date"
-    value="{{ \Carbon\Carbon::parse(old($fieldName, $defaultValue ?? \Carbon\Carbon::now()))->format(globalDateFormat()) }}"
+    value="{{ $value }}"
   />
 </div>
 @error($fieldName)
 <span class="uk-text-danger uk-text-small">
-          <strong>{{ $message }}</strong>
-        </span>
+  <strong>{{ $message }}</strong>
+</span>
 @enderror
+
+@push('scripts')
+  <script>
+    function clearDatePicker() {
+      $('#{{ $fieldName }}')[0]._flatpickr.setDate('{{ $value }}')
+    }
+  </script>
+@endpush
