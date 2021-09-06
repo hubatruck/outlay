e11ecd5449351a2a9e0b66aab15e2b1cbeff00f1
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-09-06 16:20:11 +0300

    DataTables: Add range clear button

--- resources/views/skeletons/datatables-init.blade.php
+++ resources/views/skeletons/datatables-init.blade.php
@@ -1,5 +1,13 @@
 function () {
     const api = this.api();
+    let previousRange = '';
+    const pickerTemplate =
+        '{{ __('Filter dates') }}:' +
+        '<div id="dtc-date-range" class="uk-inline uk-width-1-1 uk-form-controls">' +
+        '   <a id="dtc-reset" class="uk-form-icon uk-form-icon-flip" uk-icon="home" uk-tooltip="{{ __('Reset range') }}" data-clear></a>' +
+        '   <input id="{{ $dateRangeID }}" type="text" placeholder="{{ __('Select the date range to be shown') }}" data-input>' +
+        '</div>';
+
     /// column footer search boxes
     api.columns().every(function () {
         const column = this;
@@ -24,8 +32,7 @@ function () {
     //            });
     //        });
 
-    let previousRange = '';
-    $('#dtc-filters').append('{{ __('Filter dates') }}:<div id="dtc-date-range"> <input type="text" placeholder="{{ __('Select the date range to be shown') }}" data-input id="{{ $dateRangeID }}"></div>');
+    $('#dtc-filters').append(pickerTemplate);
     flatpickr.localize(flatpickr.l10ns.hu);
     $('#dtc-date-range').flatpickr({
         mode: 'range',
@@ -42,5 +49,9 @@ function () {
             }
         },
         locale: "{{ config('app.locale') }}"
+    });
+
+    $('#dtc-reset').click(() => {
+        api.draw();
     });
 }
