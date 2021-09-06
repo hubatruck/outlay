df0612ec8d4f57854ba1403d3fa7889c40a6ba56
hubatruck <49451870+hubatruck@users.noreply.github.com>
2021-09-06 22:07:40 +0300

    Wallet: Rename is_card column to is_public in the database
    The is_card feature wasn't used at all, so it gets replaced by the
    'publicity status'. This represents, if other users can transfer sums to
    the specified wallet.

---
+++ database/migrations/2021_09_06_220038_rename_is_card_wallet_property_to_is_public_for.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameIsCardWalletPropertyToIsPublicFor extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('wallets', function (Blueprint $table) {
+            $table->renameColumn('is_card', 'is_public');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('wallets', function (Blueprint $table) {
+            $table->renameColumn('is_public', 'is_card');
+        });
+    }
+}
